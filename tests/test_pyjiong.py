cc3bf5c66b0cbf0f091c38b92441d6f775a1ffa2
Wilhelm <email@jiong3.org>
2013-10-01 11:29:06 +0200

    tests for new functions

--- tests/test_pyjiong.py
+++ tests/test_pyjiong.py
@@ -49,12 +49,64 @@ class PyjiongTest(unittest.TestCase):
             self.assertEqual(sample_in, sample_out)
 
     def test_convert_pinyin(self):
-        num_pinyin = 'ni3 hao3'
-        mark_pinyin = 'nǐ hǎo'
+        test_list = [
+                ('ni3', 'nǐ'),
+                ('hao3', 'hǎo'),
+                ('nu:5', 'nü'),
+                ('nve5', 'nüe')
+                ]
+        for test in test_list:
+            self.assertEqual(test[1], pyjiong.pinyin_num_to_mark(test[0]))
+            self.assertEqual(pyjiong.pinyin_normalize(test[0]),
+                             pyjiong.pinyin_mark_to_num(test[1]))
+    
+    def test_split_pinyin(self):
+        test_list1 = [
+                ('yi1', 'i'),
+                ('ya2', 'ia'),
+                ('yao3', 'iao'),
+                ('ye4', 'ie'),
+                ('you1', 'iu'),
+                ('yan2', 'ian'),
+                ('yang3', 'iang'),
+                ('yin4', 'in'),
+                ('ying5', 'ing'),
+                ('yong1', 'iong'),
+                ('wu2', 'u'),
+                ('wa3', 'ua'),
+                ('wo', 'uo'),
+                ('wei4', 'ui'),
+                ('wai2', 'uai'),
+                ('wan3', 'uan'),
+                ('wen', 'un'),
+                ('wang3', 'uang'),
+                ('weng1', 'ueng'),
+                ('yu1', 'ü'),
+                ('yue3', 'üe'),
+                ('yuan2', 'üan'),
+                ('yun4', 'ün')
+                ]
+        test_list2 = [
+                ('Que4', ('q', 'üe', 4)),
+                ('Hm2', ('hm', '', 2)),
+                ('shuang1', ('sh', 'uang', 1)),
+                ('you2', ('', 'iu', 2)),
+                ('yo', ('', 'io', 5))
+                ]
+        for test in test_list1:
+            self.assertEqual(pyjiong.pinyin_split(test[0])[1], test[1])
+        for test in test_list2:
+            self.assertEqual(pyjiong.pinyin_split(test[0]), test[1])
 
-        self.assertEqual(mark_pinyin, pyjiong.pinyin_num_to_mark(num_pinyin))
-        self.assertEqual(num_pinyin[:3],
-                pyjiong.pinyin_mark_to_num(mark_pinyin[:2]))
+
+    def test_similar_pinyin(self):
+        test_list = [
+                ('feng1', 'fen'),
+                ('ding3', 'ting2'),
+                ('bang3', 'pan2')
+                ]
+        for test in test_list:
+            self.assertTrue(pyjiong.pinyin_are_similar(test[0], test[1]))
 
     def test_only_hanzi(self):
         hanzi_mix = '1234567890ß!"§$%&/()=? \
